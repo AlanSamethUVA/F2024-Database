b751e4ab9333b568eb9f666e3bf23e3b7602e074
conjware <zum9bn@virginia.edu>
2024-11-29 00:01:41 -0500

    Spring Boot Creation
    Created a spring boot project with some SQL dependencies

---
+++ P5/Databases-Project/src/test/java/com/example/Databases/Project/DatabasesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Databases.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
