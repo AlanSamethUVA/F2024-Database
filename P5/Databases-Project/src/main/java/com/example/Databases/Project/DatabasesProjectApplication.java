de0c494f5c00618b04648aa76832d0d6829521ca
Alan <cmn5et@virginia.edu>
2024-12-01 21:14:18 -0500

    Current Springboot setup

--- P5/Databases-Project/src/main/java/com/example/Databases/Project/DatabasesProjectApplication.java
+++ P5/Databases-Project/src/main/java/com/example/Databases/Project/DatabasesProjectApplication.java
@@ -1,13 +1,19 @@
 package com.example.Databases.Project;
-
+package net.codejava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DatabasesProjectApplication {
 
+	private JdbcTemplate jdbcTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DatabasesProjectApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+
 	}
 
 }
